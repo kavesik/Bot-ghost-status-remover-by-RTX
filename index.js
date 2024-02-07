83052bd9e1ac33a1f95c9276a4d541d841adaf89
kavesik <159279918+kavesik@users.noreply.github.com>
2024-02-07 16:42:52 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["ʀᴇᴋʀᴜ ᴅᴏ ʜᴘᴘʟ","ʙᴏᴛ ɢɪʟᴅɪɪ ʜᴘᴘʟ","ᴋᴀᴄᴘᴇʀ ᴛᴏ sᴢʏᴍᴘᴀɴs"];
 
 
 let currentIndex = 0;
